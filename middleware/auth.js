5ff7f30c86866f7d8028843e780ba08ec9bdc4a0
LeslyTF <lfiacre@gmail.com>
2021-11-10 16:45:49 +0100

    verif

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  console.log(req);
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, '>FKTU4*?pGN,K]h');
@@ -10,7 +9,6 @@ module.exports = (req, res, next) => {
       throw 'Invalid user ID';
     } else {
       req.userIdAddedByAuth = userId
-      
       next();
     }
   } catch {
